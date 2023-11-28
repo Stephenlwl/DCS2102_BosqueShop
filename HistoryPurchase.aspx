b3d5ab00e97d6e8718d3bd38d94517e0098ea585
Stephenlwl <137365178+Stephenlwl@users.noreply.github.com>
2023-11-28 15:12:35 +0800

    Add files via upload

---
+++ HistoryPurchase.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HistoryPurchase.aspx.cs" Inherits="DCS2102_GroupAssignment2.HistoryPurchase" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>History Purchase</title>
+    <style type="text/css">
+        #GridView1 {
+            margin: 20px auto;
+            width: 90%;
+            border-collapse: collapse;
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+
+            #GridView1 th, #GridView1 td {
+                padding: 15px;
+                border: 1px solid #ddd;
+            }
+
+            #GridView1 th {
+                background-color: #B9B4C7;
+            }
+
+            #GridView1 tbody tr:hover {
+                background-color: #f5f5f5;
+            }
+
+            #GridView1 caption {
+                font-size: 1.5em;
+                margin-bottom: 10px;
+            }
+
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+
+        /* Adjustments for responsiveness on smaller screens */
+        @media screen and (max-width: 600px) {
+            #GridView1 {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body>
+    <h1 style="text-align: center;">~ History Purchase Section ~</h1>
+    <form id="Form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="PurchaseId" HeaderText="Order ID" />
+                <asp:BoundField DataField="ItemName" HeaderText="Bosque/Flower Name" />
+                <asp:BoundField DataField="PurchaseDate" HeaderText="Purchase Date" />
+                <asp:BoundField DataField="PricePerUnit" HeaderText="Price per Unit [RM]" />
+                <asp:BoundField DataField="Quantity" HeaderText="Quantity" />
+                <asp:BoundField DataField="TotalPrice" HeaderText="Total Price [RM]" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ HistoryPurchase.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DCS2102_GroupAssignment2
+{
+    public partial class HistoryPurchase : System.Web.UI.Page
+    {
+        SqlConnection connection = new SqlConnection("Data Source=HP\\SQLEXPRESS;Initial Catalog=BouqsDB;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string username = GetCurrentUsername();
+
+                // Fetch purchase history data for the specific user
+                DataTable dt = GetPurchaseHistory(username);
+
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+
+        private DataTable GetPurchaseHistory(string username)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("PurchaseId");
+            dt.Columns.Add("ItemName");
+            dt.Columns.Add("PurchaseDate");
+            dt.Columns.Add("PricePerUnit");
+            dt.Columns.Add("Quantity");
+            dt.Columns.Add("TotalPrice");
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT purchase_id, flower_name, purchase_date, flower_unit_price, flower_quantity, flower_total_price FROM purchase_table WHERE user_name = @user_name", connection);
+            command.Parameters.AddWithValue("@user_name", username);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            string currentPurchaseId = null;
+
+            while (reader.Read())
+            {
+                string purchaseId = reader["purchase_id"].ToString();
+
+                if (purchaseId != currentPurchaseId)
+                {
+                    // Add a row for each purchase
+                    dt.Rows.Add(
+                        purchaseId,
+                        reader["flower_name"], 
+                        reader["purchase_date"], 
+                        reader["flower_unit_price"],
+                        reader["flower_quantity"], 
+                        reader["flower_total_price"] 
+                    );
+
+                    currentPurchaseId = purchaseId;
+                }
+                else
+                {
+                    // Add additional rows for the same purchase (different products)
+                    dt.Rows.Add(
+                        null, // or string.Empty depending on your needs
+                        reader["flower_name"], 
+                        reader["purchase_date"], 
+                        reader["flower_unit_price"], 
+                        reader["flower_quantity"], 
+                        reader["flower_total_price"] 
+                    );
+                }
+            }
+
+            connection.Close();
+
+            return dt;
+        }
+
+        // to get the current username
+        private string GetCurrentUsername()
+        {
+            // Retrieve the username from the user session
+            return Session["Username"] as string ?? "DefaultUsername";
+        }
+    }
+}
